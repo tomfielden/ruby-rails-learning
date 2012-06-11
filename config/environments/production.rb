b674a2dc675b15c10bd28726233986d6ec32fa91
Tom Fielden <tom@tomfielden.com>
2012-06-11 16:26:20 -0700

    Want to see the properties in Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ SampleApp::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  #config.assets.compile = false
+  config.assets.compile = true        ## TRF: so we can see the environment properties
 
   # Generate digests for assets URLs
   config.assets.digest = true
